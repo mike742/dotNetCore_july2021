c5fb04e04ace6db506b3ec2634ddb139f804fce1
Mike Zorin <mike.zorin@robertsoncollege.ca>
2021-07-21 11:37:15 -0500

    added maps

--- dotNetCore_july2021/Program.cs
+++ dotNetCore_july2021/Program.cs
@@ -6,15 +6,34 @@ namespace dotNetCore_july2021
 {
     class Program
     {
+        private readonly String[] unitsMap = new[] { 
+            "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", 
+            "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", 
+            "seventeen", "eighteen", "nineteen"
+        };
+
+        private readonly String[] tensArr = new string[] { 
+            "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninty" };
+
+        private readonly string[] suffixesArr = new string[] { "thousand", "million", "billion", 
+            "trillion", "quadrillion", "quintillion", "sextillion", "septillion", "octillion", 
+            "nonillion", "decillion", "undecillion", "duodecillion", "tredecillion", 
+            "Quattuordecillion", "Quindecillion", "Sexdecillion", "Septdecillion", 
+            "Octodecillion", "Novemdecillion", "Vigintillion" };
+
         static void ThreeToWords(string part)
         {
-
+            part = part.PadLeft(3, '0'); 
             Console.WriteLine(part);
         }
 
         static void Main(string[] args)
         {
-            string input = "1850000000000000000000000000000000";
+            char ch = 'a';
+
+            Console.WriteLine((int)ch);
+
+            string input = "18500000000000000000000000000000000";
             BigInteger bi = BigInteger.Parse(input);
 
             string num = bi.ToString("N0");
