229f9cecc95a5209c45bee6bb9dcd1a289a2085d
Mike Zorin <mike.zorin@robertsoncollege.ca>
2021-07-22 09:47:52 -0500

    added three digit logic

--- dotNetCore_july2021/Program.cs
+++ dotNetCore_july2021/Program.cs
@@ -6,25 +6,50 @@ namespace dotNetCore_july2021
 {
     class Program
     {
-        private readonly String[] unitsMap = new[] { 
+        private static readonly String[] unitsMap = new[] { 
             "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", 
             "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", 
             "seventeen", "eighteen", "nineteen"
         };
 
-        private readonly String[] tensArr = new string[] { 
-            "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninty" };
+        private static readonly String[] tensArr = new string[] {
+            "", "ten", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninty" };
 
-        private readonly string[] suffixesArr = new string[] { "thousand", "million", "billion", 
+        private static readonly string[] suffixesArr = new string[] { "", "thousand", "million", "billion", 
             "trillion", "quadrillion", "quintillion", "sextillion", "septillion", "octillion", 
             "nonillion", "decillion", "undecillion", "duodecillion", "tredecillion", 
             "Quattuordecillion", "Quindecillion", "Sexdecillion", "Septdecillion", 
             "Octodecillion", "Novemdecillion", "Vigintillion" };
 
-        static void ThreeToWords(string part)
+        static string ThreeToWords(string part)
         {
-            part = part.PadLeft(3, '0'); 
-            Console.WriteLine(part);
+            part = part.PadLeft(3, '0');
+            string res = string.Empty;
+
+            if(part[0] != '0') // 1-9
+            {
+                res += unitsMap[Convert.ToInt32(part[0].ToString())] 
+                    + " hundred";
+            }
+
+            if (part[1] == '1')
+            {
+                int ind = Convert.ToInt32(part[1].ToString() + part[2].ToString());
+                string teen = unitsMap[ind];
+
+                res += string.IsNullOrEmpty(res) ? teen : " and " + teen;
+            }
+            else
+            {
+                int ind = Convert.ToInt32(part[1].ToString());
+                int ind2 = Convert.ToInt32(part[2].ToString());
+                string tens = tensArr[ind];
+                string single = unitsMap[ind2];
+
+                res += string.IsNullOrEmpty(res) ? tens : " and " + tens + " " + single;
+            }
+
+            return res;
         }
 
         static void Main(string[] args)
@@ -33,7 +58,7 @@ namespace dotNetCore_july2021
 
             Console.WriteLine((int)ch);
 
-            string input = "18500000000000000000000000000000000";
+            string input = "829000000000000000000000000000000000";
             BigInteger bi = BigInteger.Parse(input);
 
             string num = bi.ToString("N0");
@@ -42,9 +67,10 @@ namespace dotNetCore_july2021
 
             string[] partsByThree = num.Split(",");
 
-            foreach (var part in partsByThree)
+            for(var i = 0;  i < partsByThree.Length; ++i)
             {
-                ThreeToWords (part);
+                //Console.WriteLine($"{ThreeToWords(partsByThree[i])} - {suffixesArr[partsByThree.Length - i - 1]}");
+                Console.WriteLine($"{partsByThree[i]} {ThreeToWords(partsByThree[i])}");
             }
 
             /*
