93e998ad1390a4d1baf07c81ed0053ec4e1d1cf3
Mike Zorin <mike.zorin@robertsoncollege.ca>
2021-07-28 09:46:18 -0500

    added ToXml and FromXml

--- dotNetCore_july2021/Program.cs
+++ dotNetCore_july2021/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Text.Json;
+using System.Xml.Serialization;
 
 namespace dotNetCore_july2021
 {
@@ -16,6 +17,7 @@ namespace dotNetCore_july2021
     public class Menu
     {
         public string header { get; set; }
+        [XmlElement]
         public List<Item> items { get; set; }
     }
 
@@ -60,69 +62,44 @@ namespace dotNetCore_july2021
             obj.Print();
 
             /*
-            // File f;
-            // Path p;
+            var xs = new XmlSerializer(typeof(Top));
+            string xmlFile = @"data.xml";
 
-            string file = @"d:\temp\file.txt";
-
-            Console.WriteLine(File.Exists(file));
-            Console.WriteLine(Path.GetFileName(file));
-            Console.WriteLine(Path.GetDirectoryName(file));
-            Console.WriteLine(Path.GetFileNameWithoutExtension(file));
-            Console.WriteLine(Path.GetExtension(file));
-
-            for (int i = 0; i < 5; i++)
+            using (FileStream fs = File.Create(xmlFile))
             {
-                string res = Path.GetRandomFileName();
-                Console.WriteLine("Temp path: " + res);
+                xs.Serialize(fs, obj);
             }
-
-            string file1 = @"d:\temp\file1.qwerty";
-
-            using (FileStream fs = File.Create(file1))
-            {
-                AddText(fs, "Line #1\n");
-                AddText(fs, "Line #2\n");
-                AddText(fs, "Line #3\n");
-                AddText(fs, "Line #4\n");
-            }
-
-            Console.WriteLine("----------------------------------------");
-
-            using (FileStream fs = File.OpenRead(file1))
-            {
-                byte[] bArr = new byte[1024];
-                var temp = new UTF8Encoding(true);
-
-                while (fs.Read(bArr, 0, bArr.Length) > 0)
-                {
-                    Console.WriteLine(temp.GetString(bArr));
-                }
-            }
-
-            //if(File.Exists(file1))
-            //{
-            //    File.Delete(file1);
-            //}
-
-            Console.WriteLine("----------------------------------------");
-
-            var arrStr = File.ReadAllLines(file1);
-
-            foreach (var s in arrStr)
-            {
-                Console.WriteLine(s);
-            }
-
-            // File.WriteAllLines(file1, new string[] { "Hello", "Files" });
-            File.AppendAllLines(file1, new string[] { "Hello", "Files" });
             */
+
+            ToXmlFile("sample1.xml", obj);
+
+            Console.WriteLine("==================================");
+
+            var resObj = FromXmlFile<Top>("sample1.xml");
+            resObj.Print();
+
         }
 
-        private static void AddText(FileStream fs, string line)
+        public static T FromXmlFile<T>(string file)
         {
-            byte[] arr = new UTF8Encoding(true).GetBytes(line);
-            fs.Write(arr, 0, arr.Length);
+            using (StringReader sr = new StringReader(file))
+            {
+                XmlSerializer xmls = new XmlSerializer(typeof(T));
+
+                return (T)xmls.Deserialize(sr);
+            }
         }
+
+        public static void ToXmlFile<T>(string file, T obj)
+        {
+            using (StringWriter sw = 
+                new StringWriter( new StringBuilder() ))
+            { 
+                XmlSerializer xmls = new XmlSerializer(typeof(T));
+                xmls.Serialize(sw, obj);
+                File.WriteAllText(file, sw.ToString());
+            }
+        }
+
     }
 }
