f2a47f97ec6a268bdea5e666a53bc5981246232c
Mike Zorin <mike.zorin@robertsoncollege.ca>
2021-08-03 09:43:45 -0500

    added Binary serialization

--- dotNetCore_july2021/Program.cs
+++ dotNetCore_july2021/Program.cs
@@ -3,6 +3,7 @@ using Protector;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.Json;
@@ -11,65 +12,97 @@ using static System.Console;
 
 namespace dotNetCore_july2021
 {
-    
+    public class Customer
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+        [XmlElement("creditcard")]
+        public string CardNumber { get; set; }
+        [XmlElement("password")]
+        public string Password { get; set; }
+    }
+
+    [XmlRoot("customers")]
+    public class Customers
+    {
+        [XmlElement("customer")]
+        public List<Customer> customers { get; set; }
+    }
+
+
+    [Serializable()]
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Salary { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id} {FirstName} {LastName} {Salary}";
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
+            string binaryFile = "Employee.dat";
 
-            
-
-
-            // string sk = ProtectedClass.GenerateSecretKey();
-            string sk = @"\SBO;FK`y4O_fdi8\cj=]uyKnoUC0C\<";
-
-            var res = Cripto.Cripto.EncryptString(sk, "Admin");
-
-            WriteLine(res);
-
-
-
-            WriteLine($"sk = {sk}");
-
-            string eStr = ProtectedClass.EncryptString(sk, "1234-5678-9012-3456");
-
-            WriteLine(eStr);
-
-            string dStr = ProtectedClass.DecryptString(sk, eStr);
-
-            WriteLine(dStr);
-
-            WriteLine("=================================================");
-
-
-            string password = "Admin1234";
-            string hashed = ProtectedClass.toMD5(password);
-            string hashed2 = ProtectedClass.SaltAndHash(password);
-            string hashed3 = string.Empty;
-
-            using (SHA256 sha256Hash = SHA256.Create())
+            List<Employee> employees = new List<Employee>
             {
-                hashed3 = ProtectedClass.GetHash(sha256Hash, password);
+                new Employee { Id = 101, FirstName = "Mark", LastName = "Johnson", Salary = 1850 },
+                new Employee { Id = 102, FirstName = "Lucy", LastName = "Doe", Salary = 1900 },
+                new Employee { Id = 103, FirstName = "Tracy", LastName = "Swanson", Salary = 2150 },
+                new Employee { Id = 104, FirstName = "John", LastName = "Hill", Salary = 2200 },
+            };
 
-                WriteLine($"The SHA256 hash of {password} is: {hashed3}.");
+            using (Stream st = File.Open(binaryFile, FileMode.Create))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+
+                //Employee employeeObj = new Employee { 
+                //    Id = 101, FirstName = "Mark", LastName = "Johnson", Salary = 1500
+                //};
+
+                bf.Serialize(st, employees);
             }
 
-            string userPassword = "Admin1234";
-            string hashedUserPassword = ProtectedClass.toMD5(userPassword);
-            string hashedUserPassword2 = ProtectedClass.SaltAndHash(userPassword);
 
-            WriteLine(hashed);
-            WriteLine(hashed2);
-            WriteLine(hashed3);
+            using (Stream st = File.Open(binaryFile, FileMode.Open))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
 
-            if (hashed2.Equals(hashedUserPassword2))
-            {
-                WriteLine("signed in!!!");
+                Employee emp = (Employee)bf.Deserialize(st);
+
+                WriteLine(emp);
             }
-            else
+
+
+
+                string xmlFile = "UnsecuredData.xml";
+            Customers obj = FromXmlFile<Customers>(xmlFile);
+
+            foreach (var c in obj.customers)
             {
-                WriteLine("Incorrect password!!!");
+                WriteLine(c.CardNumber + " " + c.Name);
             }
+
+
+            List<Customer> custometsList = new List<Customer>
+            {
+                new Customer { Name = "Bob Smith", CardNumber = "1234-5678-9012-3456", Password = "Pa$$w0rd"},
+                new Customer { Name = "Lucy Smith", CardNumber = "9876-6543-1234-6541", Password = "123456"},
+            };
+
+            Customers obj2 = new Customers { 
+                customers = custometsList
+            };
+
+            ToXmlFile("customers2.xml", obj2);
+
 
         }
 
